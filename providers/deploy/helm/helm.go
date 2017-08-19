72334e075a08fe44b31575dd90187aa3432bfa6a
Brian Akins <brian@akins.org>
2017-08-19 16:38:02 -0400

    just use a grpc client

--- providers/deploy/helm/helm.go
+++ providers/deploy/helm/helm.go
@@ -1,15 +1,10 @@
 package helm
 
 import (
-	"fmt"
-
 	"github.com/bakins/alecton"
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/helm/pkg/proto/hapi/release"
 	"k8s.io/helm/pkg/proto/hapi/services"
 )
@@ -114,7 +109,7 @@ func (h *Helm) ReleaseHistory(ctx context.Context, req *services.GetHistoryReque
 	if err != nil {
 		return nil, err
 	}
-
+	1
 	releases := []*release.Release{}
 
 	for _, r := range res.Releases {
@@ -133,26 +128,4 @@ func provider(config map[string]interface{}) (alecton.DeployProvider, error) {
 
 func init() {
 	alecton.RegisterDeployProvider("helm", provider)
-}
-
-// getKubeClient creates a Kubernetes config and client for a given kubeconfig context.
-func getKubeClient(context string) (*rest.Config, kubernetes.Interface, error) {
-	config, err := configForContext(context)
-	if err != nil {
-		return nil, nil, err
-	}
-	client, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, nil, fmt.Errorf("could not get Kubernetes client: %s", err)
-	}
-	return config, client, nil
-}
-
-func configForContext(context string) (*rest.Config, error) {
-	// TODO: pass in kubeconfig and actually use context
-	config, err := clientcmd.BuildConfigFromFlags("", "")
-	if err != nil {
-		return nil, fmt.Errorf("could not get Kubernetes config for context %q: %s", context, err)
-	}
-	return config, nil
 }
